7bff050d2331a78aa9432e8c8732aab8714b9e0a
debaratim2009 <141462945+debaratim2009@users.noreply.github.com>
2023-08-05 10:12:42 -0400

    Update Product.java

--- codingchallange/src/main/java/com/example/codingchallange/Entity/Product.java
+++ codingchallange/src/main/java/com/example/codingchallange/Entity/Product.java
@@ -1,6 +1,5 @@
 package com.example.codingchallange.Entity;
 
-
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -16,15 +15,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class Product {
-    public Long getProductId() {
+	public Long getProductId() {
 		return ID;
 	}
-    public Long setProductId() {
+
+	public Long setProductId() {
 		return ID;
 	}
 
@@ -53,21 +54,20 @@ public class Product {
 	}
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name = "ID",updatable = false, nullable = false, unique = true)
-    private Long ID;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID", updatable = false, nullable = false, unique = true)
+	private Long ID;
 
-    @Column(name = "Name")
-    private String name;
-    
-    
-    @Column(name = "Status")
-    private String status;
-    
-    @Column(name = "stat")
-    private String state;
-    
-    public String getState() {
+	@Column(name = "Name")
+	private String name;
+
+	@Column(name = "Status")
+	private String status;
+
+	@Column(name = "stat")
+	private String state;
+
+	public String getState() {
 		return state;
 	}
 
@@ -76,10 +76,10 @@ public class Product {
 	}
 
 	@Column(name = "price")
-    private float price;
-    
-    @Column(name = "Created")
-    private Date Created;
+	private float price;
+
+	@Column(name = "Created")
+	private Date Created;
 
 	public Date getCreationDate() {
 		return Created;
